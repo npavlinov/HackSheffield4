354ec2e0ce1f380de1f2e6f72fc9ea62822546c0
PetarSHU <b7018074@my.shu.ac.uk>
2018-10-27 22:22:37 +0300

    Clarifai API

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 let express = require('express')
 let app = express()
-
+let predict = require('./clarifai')
 
 app.use(express.static('./public'))
 app.listen(3000)
 
 console.log('Express app running on port 3000')
+console.log(predict)
 
