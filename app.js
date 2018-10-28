fb1e06be1991f9c802292bbda114af1c5bc7c315
npavlinov <nplyubomirov1@sheffield.ac.uk>
2018-10-28 01:44:31 +0100

    Working clarifai

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 let express = require('express')
+let cors = require('cors')
+const Clarifai = require('clarifai')
+
+const capp = new Clarifai.App({
+    apiKey: '1444a17f32b94bd1ab48ac7ce5e65603'
+});
 let app = express()
-let predict = require('./clarifai')
 
+app.use(cors())
+app.use(express.json())
 app.use(express.static('./public'))
-app.listen(3000)
+let promiseLink = new Promise(function(resolve, reject) {
+    app.post('/recipes', (req, res) => {
+        resolve(req.body.linkText)
+})})
+promiseLink.then(function(value) {
+    capp.models.initModel({id: Clarifai.FOOD_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
+      .then(generalModel => {
+        return generalModel.predict(value)
+      })
+      .then(response => {
+        var concepts = response['outputs'][0]['data']['concepts']
+        console.log(concepts)
+      })
+})
 
-console.log('Express app running on port 3000')
-console.log(predict)
+app.listen(3000)
 
--- clarifai.js
+++ clarifai.js
@@ -1,11 +1,13 @@
-const Clarifai = require('clarifai');
+const Clarifai = require('clarifai')
+let linkToImage = require('./app')
+
 const app = new Clarifai.App({
     apiKey: '1444a17f32b94bd1ab48ac7ce5e65603'
    });
 
 exports.predict = app.models.initModel({id: Clarifai.FOOD_MODEL, version: "aa7f35c01e0642fda5cf400f543e7c40"})
       .then(generalModel => {
-        return generalModel.predict("https://samples.clarifai.com/metro-north.jpg");
+        return generalModel.predict(linkToImage)
       })
       .then(response => {
         var concepts = response['outputs'][0]['data']['concepts']
